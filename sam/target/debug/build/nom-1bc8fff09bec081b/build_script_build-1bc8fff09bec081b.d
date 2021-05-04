9eab362bd84f16fb6efde4ec227bd5ca2e5ebb74
Error1000 <udreadanmihai@gmail.com>
2021-05-04 13:16:14 +0300

    Test programgs

--- sam/target/debug/build/nom-1bc8fff09bec081b/build_script_build-1bc8fff09bec081b.d
+++
@@ -1,5 +0,0 @@
-/home/dan/Documents/Coding/Digital/Frenzy/sam/target/debug/build/nom-1bc8fff09bec081b/build_script_build-1bc8fff09bec081b: /home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-6.1.2/build.rs
-
-/home/dan/Documents/Coding/Digital/Frenzy/sam/target/debug/build/nom-1bc8fff09bec081b/build_script_build-1bc8fff09bec081b.d: /home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-6.1.2/build.rs
-
-/home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/nom-6.1.2/build.rs:
--- sam/target/debug/build/radium-e10ea8eb436de78d/build_script_build-e10ea8eb436de78d.d
+++
@@ -1,5 +0,0 @@
-/home/dan/Documents/Coding/Digital/Frenzy/sam/target/debug/build/radium-e10ea8eb436de78d/build_script_build-e10ea8eb436de78d: /home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/radium-0.5.3/build.rs
-
-/home/dan/Documents/Coding/Digital/Frenzy/sam/target/debug/build/radium-e10ea8eb436de78d/build_script_build-e10ea8eb436de78d.d: /home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/radium-0.5.3/build.rs
-
-/home/dan/.cargo/registry/src/github.com-1ecc6299db9ec823/radium-0.5.3/build.rs:
